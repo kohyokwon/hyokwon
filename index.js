0a0838030e66956b78d50acf9681ca277aaa4adf
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-22 12:45:03 +0900

    README.md

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-const stringFive = '5';
-const numberFive = 5;
+const ellie1 = {name:'ellie'};
+const ellie2 = {name:'ellie'}
+const ellie3 = ellie1;
+console.log(ellie1 == ellie2);
+console.log(ellie1 === ellie2);
+console.log(ellie1 === ellie3);
 
-console.log(stringFive == numberFive);
-console.log(stringFive != numberFive);
-
-//strict equality
-console.log(stringFive === numberFive);
-console.log(stringFive !== numberFive);
+console.log(0 === false);
+console.log(0 === false);
+console.log('' == false);
+console.log('' === false);
+console.log(null == undefined);
+console.log(null === undefined);
