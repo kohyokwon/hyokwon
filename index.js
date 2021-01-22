84ea1eabf4e3c6b1188593a82e31a0eaf532bc12
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-22 13:31:41 +0900

    README.md

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ if (name ==='ellie'){
 }
 
 console.log(name === 'elli' ? 'hyokwon' : 'mrGo');
+
+const browser = 'IE';
+switch (browser) {
+   case 'IE':
+    console.log('go away');
+    break;
+   case 'chrome':
+    console.log('love you');
+    break;
+   case 'Firefox':
+    console.log('love you');
+   default:
+    console.log('same all');
+    break;
+}
+
+
