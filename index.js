c514b6ab46b930e99e6ab30de04120678988e292
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-26 15:55:51 +0900

    README.md

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 
-const name = 23;
-
-console.log(name === 2 ? 'yes' : 'no');
-
-
-const browser = 'chrome'
-    switch (browser) {
-        case 'IE':
-        case 'chrome':
-            console.log('go away!');
-            break;
-    }
-
     let i = 3;
-    while (i > 0) {
-        console.log(`while:${i}`);
-        i--;
-    }
-
+    
     do {
         console.log(`do while: ${i}`);
         i--;
     }   while (i > 0);
+
+    for (i = 3; i > 0; i--){
+        console.log(`for: ${i}`);
+    }
+
+    for (let i = 3; i > 0; i = i - 2){
+        console.log(`inline variable for: ${i}`);
+    }
