21fbe6f20e4c850f97831d2fd6681fde63a220a1
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-26 16:21:07 +0900

    README.md

--- index.js
+++ index.js
@@ -13,3 +13,18 @@
     for (let i = 3; i > 0; i = i - 2){
         console.log(`inline variable for: ${i}`);
     }
+
+
+    function log(message) {
+        console.log(message);
+    }
+    log('Hello@');
+    log(1234);
+
+
+    function changeName(obj) {
+        obj.name = 'coder';
+    }
+    const ellie = {name: 'ellie'};
+    changeName(ellie);
+    console.log(ellie);
