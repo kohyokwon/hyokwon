cbeb793aa420ba4f086243e9f658d4c584463bd4
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-26 16:36:37 +0900

    README.nd

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 
-    let i = 3;
-    
-    do {
-        console.log(`do while: ${i}`);
-        i--;
-    }   while (i > 0);
 
-    for (i = 3; i > 0; i--){
-        console.log(`for: ${i}`);
-    }
-
-    for (let i = 3; i > 0; i = i - 2){
-        console.log(`inline variable for: ${i}`);
-    }
-
-
-    function log(message) {
-        console.log(message);
-    }
-    log('Hello@');
-    log(1234);
-
-
-    function changeName(obj) {
-        obj.name = 'coder';
-    }
-    const ellie = {name: 'ellie'};
-    changeName(ellie);
-    console.log(ellie);
+function printAll(...args) {
+    for (const arg of args){
+        console.log(arg);
+    };
+    printAll('dream', 'coding', 'ellie');
+}
