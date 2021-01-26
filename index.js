2b501b1d44fb9bf01fa69ca11b6d3406184b2633
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-26 15:38:39 +0900

    README.md

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 
-const stringFive = '5';
-const numberFive = 5;
+const name = 23;
 
-console.log(stringFive !== numberFive);
-
-
-if (name === 'hyokwon') {
-    console.log('Wellcome, hyokwon!');
-}   else if (name === 'coder'){
-    console.log('You are amazing coder');
-} else {
-    console.log('unkwnon');
-}
+console.log(name === 2 ? 'yes' : 'no');
 
 
-const name = "coder1";
-console.log(name === 'coder1' ? 'yes' : 'no');
+const browser = 'chrome'
+    switch (browser) {
+        case 'IE':
+        case 'chrome':
+            console.log('go away!');
+            break;
+    }
+
+    let i = 3;
+    while (i > 0) {
+        console.log(`while:${i}`);
+        i--;
+    }
+
+    do {
+        console.log(`do while: ${i}`);
+        i--;
+    }   while (i > 0);
