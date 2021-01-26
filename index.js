bb21c64596764a479fed88b38f29bc25f3a2c982
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-26 19:20:29 +0900

    README.md

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 
-let globalMessage = 'global';
-function printMessage() {
-    let message = 'hello';
-    console.log(message);
-    console.log(globalMessage);
-}
-printMessage();
-
-function sum(a, b) {
-    return a + b;
-}
-
-const result = sum(1, 2);
-console.log(`sum: ${sum(1, 2)}`);
-
-
-function upgradeUser(user) {
-    if (user.point <= 10) {
-        return;
+function randomQuiz(answer, printYes, printNo){
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
     }
 }
+
+const printYes = function() {
+    console.log('Yes!');
+};
+
+const printNo = function hyokwon() {
+    console.log('No!'); 
+};
+
+randomQuiz('wrong', printYes, printNo);
+
+    
