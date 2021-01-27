6cd634ca9d4a81f9896d858d199bd83ba6a3acfa
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-27 15:48:28 +0900

    README.md

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function printAll(...args) {
-   for (const arg of args) {
-       console.log(arg);
-   }
-}
-printAll('dream', 'coding', 'ellie');
+
+
+
+
+const printAgain = 'print';
+console.log(printAgain);
