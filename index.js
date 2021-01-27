4f6b5d07a2e5859baab3a5924f970697ee33dec4
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-27 13:09:22 +0900

    README.md

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
-for (i = 3; i > 0; i--) {
-    console.log(`forL ${i}`);
-}
-
-for (let i = 4; i > 0; i = i - 2) {
-    console.log(`inline variable for: ${i}`);
-}
-
-for (let i = 0; i < 10; i++){
-    for (let j = 0; j < 10; j++){
-        console.log(`i: ${i}, j:${j}`);
-    }
+function log (message) {
+    console.log(message);
 }
