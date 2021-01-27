b69fa4d9c011301b545dd7ea7d538f1d2d47b399
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-27 11:47:32 +0900

    README.md

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 
-let x = 3;
-let y = 6;
-console.log(x += y);
-console.log(10 < 6);
+const stringFive = 7;
+const numberFive = 5;
 
-const value1 = false;
-const value2 = 4 < 2;
-console.log(`or: ${value1 || value2 || check()}`);
-function check() {
-    for (let i = 0; i < 2; i++) {
-        console.log('^^');
-        return true;
-    }
-}
+console.log(stringFive !== numberFive);
 
-console.log(!value1);
