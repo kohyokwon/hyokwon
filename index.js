7e884c3de078ed9df371db706cdfe68cc02984d0
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-27 12:00:41 +0900

    README.md

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const numberFive = 5;
 
 console.log(stringFive !== numberFive);
 
+const name = 'hyokwon';
+if (name === 'hyokwon') {
+    console.log('Wellcome, hyokwon');
+} else if (name ==='coder') {
+    console.log('you are amazing coder');
+} else {
+    console.log('unkwnon');
+}
+
+console.log(name !== 'hyokwon' ? 'Yes' : 'No');
