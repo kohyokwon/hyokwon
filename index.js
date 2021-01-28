91387d8528c8ab3f859a662d5f3fee064663fdc1
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-28 22:00:21 +0900

    README.md

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+
+
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++){
+        console.log(args[i]);
+    }
+}
+
+printAll('dream', 'coding', 'hyokown');
+
+let i = 4;
+console.log(i);
