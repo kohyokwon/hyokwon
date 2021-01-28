6bc02eed71010ca4f02a319211f9f9fe1395c81c
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-28 20:34:01 +0900

    README.md

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
+'use strict'
 
-console.log('my' + ' cat');
-console.log('1' + 2);
-console.log();
-
-console.log(1 + 1);
-console.log(1 + 2);
-
-let x = 3;
-let y = 6;
-
-console.log(x += y);
-
-const value1 = false;
-const value2 = 4 < 2;
-
-console.log(`and: ${value1 && value2 && check()}`)
-function check(){
-    for (let i = 0; i < 10; i++) {
-        console.log('hyokown');
-        return true;
-    }
-}
+const name = 'hyokwon2';
+ if (name === 'hyokwon'){
+     console.log('Welcome, hyokwon!');
+ } else if(name === 'coder') {
+     console.log('you are amazing coder');
+ } else {
+     console.log('unkwnon');
+ }
 
 
-    console.log(!value2);
+ console.log(name === 'hyokwon2' ? 'Yes' : 'No');
