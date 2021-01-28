23e33ae200ba13d750bf3716526329c62e6b3f51
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-28 20:02:15 +0900

    README.md

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+'use strict';
+
+let globalName = 'global name'
+{
+    let name = 'hyokwon';
+    console.log(name);
+    console.log(globalName);
+}
+
+console.log(name);
 
 
+{
+    console.log(jmt);
+    var jmt ='hyokwon';
+}
 
+let infinity = 1 /0;
+const negativeInfinity = -1 / 0;
 
-const printAgain = 'print';
-console.log(printAgain);
