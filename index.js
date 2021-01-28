1a92d80bfa7097913629b9ca6d50b371913d2a59
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-28 21:34:10 +0900

    REAME.md

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-for (let i = 3; i > 0; i = i - 2){
-    console.log(`inline variable for:${i}`);
-}
 
-for (let i = 0; i < 10; i++){
-    for (let j = 0; j < 10; j++){
-        console.log(`i: ${i}, j: ${j}`);
-    }
-}
