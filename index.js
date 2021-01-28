7eb07c052f2abdbc2300b17adc2c4b1293ff6bff
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-28 20:19:44 +0900

    README.md

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ console.log(x += y);
 const value1 = false;
 const value2 = 4 < 2;
 
-console.log(`or: ${value1 || value2 || check()}`);
-function check() {
-    for (let i = 0; i < 10; i++){
-        console.log('hyokwon');
+console.log(`and: ${value1 && value2 && check()}`)
+function check(){
+    for (let i = 0; i < 10; i++) {
+        console.log('hyokown');
         return true;
     }
 }
+
+
+    console.log(!value2);
