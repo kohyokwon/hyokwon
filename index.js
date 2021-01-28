83be0728c2b617533b8a4c03de6a17e77e006ea0
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-28 20:13:14 +0900

    README.md

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-'use strict';
 
-let globalName = 'global name'
-{
-    let name = 'hyokwon';
-    console.log(name);
-    console.log(globalName);
+console.log('my' + ' cat');
+console.log('1' + 2);
+console.log();
+
+console.log(1 + 1);
+console.log(1 + 2);
+
+let x = 3;
+let y = 6;
+
+console.log(x += y);
+
+const value1 = false;
+const value2 = 4 < 2;
+
+console.log(`or: ${value1 || value2 || check()}`);
+function check() {
+    for (let i = 0; i < 10; i++){
+        console.log('hyokwon');
+        return true;
+    }
 }
-
-console.log(name);
-
-
-{
-    console.log(jmt);
-    var jmt ='hyokwon';
-}
-
-let infinity = 1 /0;
-const negativeInfinity = -1 / 0;
-
