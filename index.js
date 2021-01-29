ffc2ed65642840b2701b1bec27f9af12bb931269
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-29 18:26:21 +0900

    README.md

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
-function hyokwon(a, b) {
-    return a + b;
-}
-hyokwon(1, 3);
-console.log(`1 + 2 = ${hyokwon(1, 3)}`);
+  const simplePrint = ()=> console.log('simplePrint');
 
+  const add = function(a, b) {
+      return a + b;
+  }
 
+  const add = (a, b) => a + b;
 
+(function hello() {
+    console.log('IIFE');
+})();
+  
