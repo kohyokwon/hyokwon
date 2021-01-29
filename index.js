494c720ef6b5cfe53c7e662fb94337d41aef784e
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-30 00:13:25 +0900

    README.md

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-class Shape {
-    constructor(width, height, color){
-        this.width = width;
-        this.height = height;
-        this.color = color;
-    }
 
-    draw() {
-        console.log(`drawing ${this.color} color!`);
-    }
-    getArea() {
-        return this.width * this.height;
+function calculate(command, a, b) {
+    switch (command) {
+        case 'add':
+            return a + b;
+        
     }
 }
-
-class Rectangle extends Shape {}
-class Triangle extends Shape {
-    draw() {
-        super.draw();
-        console.log('hyokwon');
-    }
-
-    getArea() {
-        return (this.width * this.height) / 2;
-    }
-}
-
-const rectangle = new Rectangle(20, 20, 'blue');
-rectangle.draw();
-console.log(rectangle.getArea());
-const triangle = new Triangle(20, 20, 'red');
-triangle.draw();
-console.log(triangle.getArea());
