8720cb05f5d1d478644f1cc349942eedab7fef98
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-29 18:28:29 +0900

    README.md

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 
-  const simplePrint = ()=> console.log('simplePrint');
-
-  const add = function(a, b) {
-      return a + b;
-  }
-
-  const add = (a, b) => a + b;
-
-(function hello() {
-    console.log('IIFE');
-})();
-  
+class person {
+    constructor(name, age){
+        this.name = name;
+        this.age
+    }
+}
