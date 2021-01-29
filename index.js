d8800c48d2892db3fda79b821b47bd914f1fe699
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-29 18:42:44 +0900

    README.md

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ class person {
         this.name = name;
         this.age
     }
+    speak() {
+        console.log(`${this.name}: Hello!`);
+    }
 }
+const hyokwon = new person ('hyokwon', 20);
+console.log(hyokwon.name);
+console.log(hyokwon.age);
+hyokwon.speak();
+
