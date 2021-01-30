dca3340cae8b30eb5eace8ce9062ec403eba5448
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-31 00:22:47 +0900

    README.md

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 
-
-function calculate(command
-    , a, b) {
-    switch (command) {
-        case 'add':
-            return a + b;
-        default: 
-            throw Error('unkonwn command');
-    }
-}
-
-console.log(calculate('add', 2, 3));
+console.log('name' in ellie);
