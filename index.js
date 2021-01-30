e7ceb4c8cefc4f80d9f3afab0fe6ee8ba384b2a7
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-01-30 18:58:46 +0900

    READNE.md

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 
-class person {
-    constructor(name, age){
-        this.name = name;
+class User {
+    constuctor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
     }
-    speak() {
-        console.log(`${this.name}: hello!`);
+    get age() {
+        return this._age;
     }
+    set age(value){
+        if(value < 0);
+        this._age = 3;
+    } 
 }
 
-const ellie = new person('ellie', 20);
-console.log(ellie.name);
-console.log(ellie.age);
-ellie.speak();
+const user1 = new User('steve', 'Job', -1);
+console.log(user1.age);
