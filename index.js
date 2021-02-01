7f7806867897abc321025c287e3734999301cec4
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-02 01:44:14 +0900

    README.md

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-}
-const person4 = new Person('hyokwon', 30);
-console.log(person4);
+const array = [1, 2, 3, 4, 5];
+ for(value of array) {
+     console.log(value);
+ }
