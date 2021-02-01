d5491ef367586e6cffd25f9718db9dac341818b2
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-01 13:14:18 +0900

    README.md

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 
-let i = 3;
-console.log(i);
+const fruits = ['🍎', '🍌'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
