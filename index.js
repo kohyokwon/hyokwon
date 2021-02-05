25732193f3d6f382769f1b5182b07372b2ed6d30
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-05 18:49:01 +0900

    README.md

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
-const fruits = '🍕, 🌯, 🥪, 🌮';
-const result = fruits.split();
-console.log(result);
+class User {
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastNmae;
+    }
+
+get age()  {
+    return this._age = value;
+    }
+
+set age(value) {
+    this._age = value;
+    }
+}
+
+const user1 = new User('steve', 'job', -1);
+console.log(user1.age);
