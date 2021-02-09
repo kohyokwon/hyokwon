3d05a82ea6303e7d89f722e0be80f13871634603
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-09 12:38:41 +0900

    README.md

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 
-const array = [1, 2, 3, 4, 5];
-const result = array.slice(2, 5)
-console.log(result);
-console.log(array);
+class Student {
+    constructor(name, age, enrolled, score) {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+}
+
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88),
+];
+
+const result = students.find(function (studt, iex) {
+    console.log(studt, iex);
+});
