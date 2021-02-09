2c32faf1eb409382ce6124b5448474a7e8961e17
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-09 12:59:59 +0900

    README.md

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 
-class Student {
-    constructor(name, age, enrolled, score) {
-        this.name = name;
-        this.age = age;
-        this.enrolled = enrolled;
-        this.score = score;
-    }
-}
-
-const students = [
-    new Student('A', 29, true, 45),
-    new Student('B', 28, false, 80),
-    new Student('C', 30, true, 90),
-    new Student('D', 40, false, 66),
-    new Student('E', 18, true, 88),
-];
-
-const result = students.find(function (studt, iex) {
-    console.log(studt, iex);
-});
+let json = JSON.stringify(true);
+console.log(json);
