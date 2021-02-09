997616473189bec84915034612a5d42547bba90d
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-09 17:38:43 +0900

    README.md

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 
-const rabbit = {
-    name: 'tori',
-    color: 'white',
-    size: null,
-    birthDate: new Date(),
-    jump:() => {
-        console.log(`${name} can jump!`);
-    },
-};
-
-json = JSON.stringify(rabbit);
-console.log(json);
-
-json = JSON.stringify(rabbit, (key, value) => {
-    console.log(`key: ${key}, value: ${value}`);
-    return key === 'name' ? 'ellie' : value;
-});
-console.log(json);
-
-console.clear();
-json = JSON.stringify(rabbit);
-const obj = JSON.parse(json);
-console.log(obj);
-rabbit.jump();
-obj.jump();
+console.log('1');
+setTimeout(function() {
+    console.log('2');
+}, 1000);
+console.log('3');
