7b241b67dd12eadcac60f89f96453e878653293f
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-09 16:59:45 +0900

    README.md

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ const rabbit = {
 
 json = JSON.stringify(rabbit);
 console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'ellie' : value;
+});
+console.log(json);
+
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json);
+console.log(obj);
+rabbit.jump();
+obj.jump();
