c9fd4f773cd89ea135b84a79ce04625cc2b31c4c
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-09 13:55:07 +0900

    README.md

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
-let json = JSON.stringify(true);
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump:() => {
+        console.log(`${name} can jump!`);
+    },
+};
+
+json = JSON.stringify(rabbit);
 console.log(json);
