ddb9f40849e61bc0b2fae394a2c1141ee50b8a76
hyokwon <72539076+kohyokwon@users.noreply.github.com>
2021-02-15 07:54:01 +0900

    README.md

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ setTimeout(() =>
     console.log('2')
 , 1000);
 console.log('3');
+
+console.log('1');
+setTimeout(() => 
+    console.log('2')
+, 1000);
+console.log('3');
+
+console.log('1');
+setTimeout(() => 
+    console.log('2')
+, 1000);
+console.log('3');
